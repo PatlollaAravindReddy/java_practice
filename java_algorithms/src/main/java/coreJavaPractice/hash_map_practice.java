9d1ca28b626fa3f9b7743acc5961a90898c1dd78
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2024-09-22 21:21:16 -0500

    binarySearch

--- java_algorithms/src/main/java/coreJavaPractice/hash_map_practice.java
+++ java_algorithms/src/main/java/coreJavaPractice/hash_map_practice.java
@@ -1,3 +1,5 @@
+package coreJavaPractice;
+
 import java.util.HashMap;
 import java.util.Map;
 
---
+++ java_algorithms/src/main/java/dsa/binarySearch.java
@@ -0,0 +1,23 @@
+package dsa;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] data = {1,2,4,6,8,10,12,14,16};
+        int no = 16;
+        System.out.println(findNumber(no, 0, data.length , data));
+    }
+
+    static int findNumber(int no, int start, int end, int[] data) {
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (data[mid] < no) {
+                start = mid + 1;
+            } else if (data[mid] > no) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
