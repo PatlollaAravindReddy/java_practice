cbf38c8bcdb666864d76e72427ebce2c472fdc96
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2024-09-29 20:01:49 -0500

    LinkedList

--- java_algorithms/src/main/java/dsa/LL.java
+++ java_algorithms/src/main/java/dsa/LL.java
@@ -18,6 +18,71 @@ public class LL {
         size++;
     }
 
+    public void insertLast(int value) {
+        Node node = new Node(value);
+        if(size == 0) {
+            insertFirst(value);
+            return;
+        }
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int value, int index) {
+        if(size == 0) {
+            insertFirst(value);
+        }
+        if(size + 1 == index) {
+            insertLast(value);
+        }
+        Node temp = head;
+        int i =0;
+        while(i != index - 1) {
+            temp = temp.next;
+            i++;
+        }
+        Node tempNode = new Node(value, temp.next);
+        temp.next = tempNode;
+        size++;
+    }
+
+    public void deleteFirst() {
+        Node node = head;
+        head = node.next;
+        if(head == null) {
+            tail = null;
+        }
+        size--;
+    }
+
+    public void deleteLast() {
+        Node temp = head;
+        for(int i = 0;i < size - 2; i++) {
+            temp = temp.next;
+        }
+        tail = temp;
+        tail.next = null;
+        size--;
+    }
+
+    public void delete(int index) {
+        if(size == 0) {
+            deleteFirst();
+            return;
+        }
+        if(size == index) {
+            deleteLast();
+            return;
+        }
+        Node temp = head;
+        for(int i = 0; i < index - 2; i++) {
+            temp = temp.next;
+        }
+        Node tempNode = temp.next;
+        temp.next = tempNode.next;
+        size--;
+    }
     public void display() {
         Node node = head;
         while (node != null) {
--- java_algorithms/src/main/java/dsa/LinkedListPractice.java
+++ java_algorithms/src/main/java/dsa/LinkedListPractice.java
@@ -5,6 +5,12 @@ public class LinkedListPractice {
         LL llist = new LL();
         llist.insertFirst(2);
         llist.insertFirst(4);
+        llist.insertLast(6);
+        llist.insertLast(8);
+        llist.insert(10, 2);
+        llist.deleteFirst();
+        llist.deleteLast();
+        llist.delete(2);
         llist.display();
     }
 }
