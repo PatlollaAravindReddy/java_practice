4b57d356e9eedfbb5390a3dda00398ba8c0a682b
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2024-10-04 19:36:18 -0500

    Update MergeTwoSortedList_21.java

--- java_algorithms/src/main/java/leetCode/MergeTwoSortedList_21.java
+++ java_algorithms/src/main/java/leetCode/MergeTwoSortedList_21.java
@@ -1,7 +1,34 @@
 package leetCode;
 
 public class MergeTwoSortedList_21 {
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        public static void main(String[] args) {
+
+            ListNode list1 = new ListNode(1);
+            list1.next = new ListNode(2);
+            list1.next.next = new ListNode(4);
+
+            ListNode list2 = new ListNode(1);
+            list2.next = new ListNode(3);
+            list2.next.next = new ListNode(4);
+
+            // Merging the lists
+            ListNode mergedList = mergeTwoLists(list1, list2);
+
+            // Printing the merged linked list
+            printList(mergedList);
+        }
+
+        // Helper function to print the linked list
+        public static void printList(ListNode head) {
+            ListNode current = head;
+            while (current != null) {
+                System.out.print(current.val + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
             ListNode dummy = new ListNode(0);
             ListNode current = dummy;
             while(list1 != null && list2 != null) {
