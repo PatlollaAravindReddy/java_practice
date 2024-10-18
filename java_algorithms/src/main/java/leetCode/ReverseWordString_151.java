108082773caaa5b0f56d337163309ca54d9ed630
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2024-10-17 21:29:18 -0500

    Create ReverseWordString_151.java

---
+++ java_algorithms/src/main/java/leetCode/ReverseWordString_151.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+public class ReverseWordString_151 {
+    public static void main(String[] args) {
+        String str = "  hello world  ";
+        System.out.println(reverseWords(str));
+    }
+
+    public static String reverseWords(String s) {
+        s = s.trim();
+        String[] data = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = data.length - 1; i >= 0; i--) {
+            if(data[i].length() != 0) {
+                sb.append(data[i]);
+                if(i != 0) {
+                    sb.append(" ");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
