9ec7eae3a4894e07e4167615a8a6c5ec42d9d9a5
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2025-01-08 18:52:56 -0500

    rearranged

--- java_algorithms/src/main/java/dsa/searchalgorithms/BinarySearch.java
+++ java_algorithms/src/main/java/dsa/searchalgorithms/BinarySearch.java
@@ -1,4 +1,4 @@
-package dsa;
+package dsa.searchalgorithms;
 
 public class BinarySearch {
     public static void main(String[] args) {
--- java_algorithms/src/main/java/dsa/searchalgorithms/LinearSearch.java
+++ java_algorithms/src/main/java/dsa/searchalgorithms/LinearSearch.java
@@ -1,4 +1,4 @@
-package dsa;
+package dsa.searchalgorithms;
 
 public class LinearSearch {
     public static void main(String[] args) {
