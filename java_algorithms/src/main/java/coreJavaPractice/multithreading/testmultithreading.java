ba9443df2c425c266271b3c3c90d8f3d5868ed35
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2025-01-29 23:23:39 -0500

    Update testmultithreading.java

--- java_algorithms/src/main/java/coreJavaPractice/multithreading/testmultithreading.java
+++ java_algorithms/src/main/java/coreJavaPractice/multithreading/testmultithreading.java
@@ -8,15 +8,32 @@ public class testmultithreading {
         Thread t1 = new Thread();
         t1.start();
         System.out.println(Thread.activeCount());
-        Employee ee = new Employee();
+        Employee ee = new Employee("T1");
         ee.start();
+        Manager mn = new Manager("T2");
+        mn.start();
     }
 }
 
 class Employee extends Thread {
+    public Employee(String name) {
+        super(name);
+    }
 
     @Override
     public void run() {
-        System.out.println("hello");
+        System.out.println("Hello : " + Thread.currentThread().getName());
+    }
+}
+
+class Manager extends Thread {
+
+    public Manager(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hi : " + Thread.currentThread().getName());
     }
 }
