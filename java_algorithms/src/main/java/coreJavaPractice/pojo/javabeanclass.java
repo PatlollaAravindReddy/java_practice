bad3217ce2d5c1229aa42a0ab7e37c59be623457
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2025-01-31 16:01:35 -0500

    java bean class

---
+++ java_algorithms/src/main/java/coreJavaPractice/pojo/javabeanclass.java
@@ -0,0 +1,38 @@
+package coreJavaPractice.pojo;
+
+import java.io.Serializable;
+
+public class javabeanclass implements Serializable {
+
+    public javabeanclass() {
+
+    }
+
+    private int id;
+    private String name;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
--- java_algorithms/src/main/java/coreJavaPractice/pojo/mainprogram.java
+++ java_algorithms/src/main/java/coreJavaPractice/pojo/mainprogram.java
@@ -8,5 +8,13 @@ public class mainprogram {
         ee.setSalary(456789);
 
         System.out.println(ee.getId() + " - " + ee.getName() + " - " + ee.getSalary() );
+
+
+        javabeanclass jb = new javabeanclass();
+        jb.setId(2);
+        jb.setName("sai");
+        jb.setSalary(456768);
+
+        System.out.println(jb.getId() + " - " + jb.getName() + " - " + jb.getSalary() );
     }
 }
