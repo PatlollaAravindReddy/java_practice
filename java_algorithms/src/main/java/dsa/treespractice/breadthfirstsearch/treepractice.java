3891ad29a830fe63b55f7528f5368807e1616909
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2025-02-17 19:12:57 -0500

    Update treepractice.java

--- java_algorithms/src/main/java/dsa/treespractice/breadthfirstsearch/treepractice.java
+++ java_algorithms/src/main/java/dsa/treespractice/breadthfirstsearch/treepractice.java
@@ -3,39 +3,6 @@ package dsa.treespractice.breadthfirstsearch;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class TreeNode {
-    int val;
-
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {};
-    TreeNode(int val) {this.val = val;}
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-
-    void display(TreeNode tn) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(tn);
-        while(!queue.isEmpty()) {
-            int levelsize = queue.size();
-            while(levelsize > 0) {
-                TreeNode node = queue.poll();
-                System.out.println(node.val + " ");
-                if(node.left != null) {
-                    queue.add(node.left);
-                }
-                if(node.right != null) {
-                    queue.add(node.right);
-                }
-                levelsize--;
-            }
-            System.out.println();
-        }
-    }
-}
 public class treepractice {
     public static void main(String[] args) {
         TreeNode tn = new TreeNode(10);
