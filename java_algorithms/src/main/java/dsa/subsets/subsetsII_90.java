b294cc3506e43b6a289f12810c13a4c20c2bf9a8
PatlollaAravindReddy <patlollaaravind31@gmail.com>
2025-02-21 18:56:55 -0500

    subsetsII_90

---
+++ java_algorithms/src/main/java/dsa/subsets/subsetsII_90.java
@@ -0,0 +1,30 @@
+package dsa.subsets;
+
+import java.util.*;
+
+public class subsetsII_90 {
+    public static void main(String[] args) {
+        int[] data = {1, 2, 2};
+        subsetHelper(data);
+    }
+
+    static void subsetHelper(int[] data) {
+        Arrays.sort(data);
+        List<List<Integer>> ll = new ArrayList<>();
+        ll.add(new ArrayList<>());
+        int startIndex = 0, endIndex = 0;
+        for(int i = 0; i < data.length; i++) {
+            startIndex = 0;
+            int size = ll.size();
+            if(i > 0 && data[i] == data[i - 1]) {
+                startIndex = endIndex + 1;
+            }
+            endIndex = ll.size() - 1;
+            for(int j = startIndex ; j <= endIndex; j++) {
+                List<Integer> ll2 = new ArrayList<>(ll.get(j));
+                ll2.add(data[i]);
+                ll.add(ll2);
+            }
+        }
+    }
+}
